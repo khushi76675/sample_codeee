fe2fd7f5004555efd86264b936575fea76d5de35
Khushi Sharma <khushisharma9787@gmail.com>
2025-01-26 08:34:52 +0530

    Initial commit

---
+++ cal.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("khushuu calculator's")
+root.geometry("450x550")
+root.configure(bg="pink")
+root.resizable(False,True)
+root.iconphoto(True, PhotoImage(file="images/calculator.png"))
+root.mainloop()
